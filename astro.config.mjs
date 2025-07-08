23bc360a261b04ad2c147da722ec0bb03a9f7eb5
rogerogers <rogers@rogerogers.com>
2025-07-08 23:15:05 +0800

    Initial Commit
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,46 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightBlog from "starlight-blog";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://docs.rogerogers.com",
+  vite: {
+    plugins: [devtoolsJson()],
+  },
+  integrations: [
+    starlight({
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
+      plugins: [starlightBlog()],
+      title: "Rogerogers",
+      logo: {
+        src: "./src/assets/logo.svg",
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/rogerogers",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/blog/about.md
@@ -0,0 +1,31 @@
+---
+title: 关于我
+date: 2025-07-08
+---
+
+大家好！我是 Rogerogers。
+
+我是一名全栈开发工程师，同时也是 DevOps 领域的从业者。我在开发领域深耕多年，积累了丰富的经验。
+
+喜欢解决各种技术问题，能够应对各种复杂的开发任务。
+
+我非常热爱开源，始终坚信开源能够推动技术的发展，也积极参与开源项目的贡献。目前是 [CloudWeGo](https://github.com/cloudwego) 社区的成员，网站项目的 Approver。
+
+Hello everyone! I'm Rogerogers, and my full English name is Rogers David.
+
+I'm a full-stack development engineer and also a practitioner in the DevOps
+field. I have been deeply involved in the development field for many years
+and have accumulated rich experience.
+
+I enjoy solving various technical problems and am capable of handling
+various complex development tasks.
+
+I'm very passionate about open source. I firmly believe that open source can
+promote the development of technology and actively participate in the
+contribution of open source projects. Currently, I'm a member of the [CloudWeGo](https://github.com/cloudwego) community and an Approver of the website project.
+
+In life, I like DC comics. Those wonderful stories and distinct characters
+always immerse me and bring me a lot of creative inspiration.
+
+Looking forward to continuing to move forward on the road of technology in
+the future and constantly break through myself!
